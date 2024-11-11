bbd5a49ad1db4d5105547c6275f5abc25f012c40
QureshiSameet <sameet5071@gmail.com>
2024-11-11 22:33:37 +0400

    Add files via upload

---
+++ 1. Coding is Cool.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 11 18:16:34 2024
+
+@author: DELL
+"""
+
+# TAKE 3 VARIABLES (WORD1,WORD2,WORD3) AND ASSIGN THEM
+word1 = "Coding"
+word2 = "is"
+word3 = "Cool"
+# THE 3 VARIABLES (WORD1,WORD2,WORD3) ARE INTEGRATED IN A SINGLE STRING. WE USED + OPERATOR TO JOIN THEN AND USED " " TO ADD SPACE. 
+print(word1+" "+word2+" "+word3) 
